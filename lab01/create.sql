e96cbfc95682932c5f1debf628fd7967321383cb
geoHeil <e1225063@student.tuwien.ac.at>
2015-09-21 13:44:36 +0200

    update queries

--- lab01/create.sql
+++ lab01/create.sql
@@ -29,9 +29,41 @@ CREATE TABLE subtopic (
   subTopic   INTEGER REFERENCES topic (topic_id)
 );
 
+-- assumption each expert writes a own recommendation
 CREATE TABLE recommendation (
   recommendation_id INTEGER PRIMARY KEY DEFAULT nextval('seq_recommendation'),
   text              VARCHAR(1000),
   recomendededBy    INTEGER REFERENCES expert (expert_id),
-  isRrecomendeded INTEGER REFERENCES expert(expert_id)
+  isRrecomendeded   INTEGER REFERENCES expert (expert_id)
 );
+
+
+-- add content into the table
+INSERT INTO expert (email, name, address, description)
+VALUES ('e1@expert.com', 'e1name', 'e1address', '<em>some xml description</em>');
+INSERT INTO expert (email, name, address, description)
+VALUES ('e2@expert.com', 'e2name', 'e2address', '<em>some xml description</em>');
+INSERT INTO expert (email, name, address, description)
+VALUES ('e3@expert.com', 'e3name', 'e3address', '<em>some xml description</em>');
+
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading1', 'text1');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area2', 2, 'heading2', 'text2');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading3', 'text3');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading4', 'text4');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading5', 'text5');
+
+INSERT INTO expert_topic (expert_id, topic_id) VALUES (1, 1);
+INSERT INTO expert_topic (expert_id, topic_id) VALUES (1, 2);
+INSERT INTO expert_topic (expert_id, topic_id) VALUES (1, 3);
+INSERT INTO expert_topic (expert_id, topic_id) VALUES (2, 2);
+INSERT INTO expert_topic (expert_id, topic_id) VALUES (2, 3);
+INSERT INTO expert_topic (expert_id, topic_id) VALUES (3, 2);
+INSERT INTO expert_topic (expert_id, topic_id) VALUES (3, 1);
+
+INSERT INTO subtopic (superTopic, subTopic) VALUES (1, 3);
+INSERT INTO subtopic (superTopic, subTopic) VALUES (1, 4);
+INSERT INTO subtopic (superTopic, subTopic) VALUES (4, 5);
+
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 1, 3);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 1, 2);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 2, 3);
--- lab01/queries.sql
+++ lab01/queries.sql
@@ -1,22 +1,64 @@
 -- Queries
 
 -- Query A
-
 SELECT
   e.expert_id,
-  e.name,
-  ea.name
+  e.name
 FROM expert e
-  JOIN expert_expertise_area eea
-    ON e.expert_id = eea.expert_id
-  JOIN expertise_area ea
-    ON eea.expertise_area_id = ea.expertise_area_id
-WHERE ea.expertise_area_id = X;
+  JOIN expert_topic et ON e.expert_id = et.expert_id
+  JOIN topic t ON e.expert_id = t.originator
+WHERE t.expertise_area = 'area2';
 
 -- Query B
+WITH subTopicIds AS (
+    SELECT
+      t.topic_id,
+      st.subtopic
+    FROM topic t
+      JOIN subtopic st ON t.topic_id = st.supertopic
+    --   specify topic Y here
+    WHERE t.topic_id = 1
+)
+SELECT t.expertise_area
+FROM topic t
+  JOIN subTopicIds ON t.topic_id = subTopicIds.subtopic;
 
 -- Query C
+WITH expertAreaY AS (
+    SELECT
+      e.expert_id,
+      e.name
+    FROM expert e
+      JOIN expert_topic et ON e.expert_id = et.expert_id
+      JOIN topic t ON e.expert_id = t.originator
+    --       specify area Y here
+    WHERE t.expertise_area = 'area2'
+)
+
+SELECT DISTINCT
+  e.name,
+  e.expert_id
+FROM expert e
+  JOIN recommendation r ON e.expert_id = r.isrrecomendeded
+  JOIN expertAreaY eAY ON eAY.expert_id = e.expert_id
+--       choose expert X here
+WHERE r.recomendededby = 1;
 
 -- Query D
+WITH expertRecommendedByX AS (
+    SELECT DISTINCT e.expert_id
+    FROM expert e
+      JOIN recommendation r ON e.expert_id = r.isrrecomendeded
+    --   JOIN expertAreaY eAY ON eAY.expert_id = e.expert_id
+  --       choose expert X here
+    WHERE r.recomendededby = 1
+)
+SELECT DISTINCT r.isrrecomendeded
+FROM expertRecommendedByX eX
+  JOIN recommendation r ON eX.expert_id = r.recomendededby
+  JOIN expert_topic et ON r.isrrecomendeded = et.expert_id
+  JOIN topic t ON t.topic_id = et.topic_id
+--       specify area Y here
+WHERE t.expertise_area = 'area1';
 
 -- Query E
