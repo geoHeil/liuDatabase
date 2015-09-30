4de3d681d572934944c75f2e14358cba1df366b9
geoHeil <e1225063@student.tuwien.ac.at>
2015-09-30 20:56:32 +0200

    reset conversion to mysql - as we decided to use postgres

--- lab01/create.sql
+++ lab01/create.sql
@@ -1,54 +1,41 @@
-DROP TABLE IF EXISTS recommendation;
-DROP TABLE IF EXISTS subtopic;
-DROP TABLE IF EXISTS expert_topic;
-DROP TABLE IF EXISTS topic;
-DROP TABLE IF EXISTS expert;
-
+CREATE SEQUENCE seq_expert;
+CREATE SEQUENCE seq_recommendation;
+CREATE SEQUENCE seq_topic;
 
 CREATE TABLE expert (
-  expert_id   INTEGER AUTO_INCREMENT,
+  expert_id   INTEGER PRIMARY KEY DEFAULT nextval('seq_expert'),
   email       VARCHAR(200),
   name        VARCHAR(200),
   address     VARCHAR(500),
-  description VARCHAR(1000),
-  PRIMARY KEY (expert_id)
+  description VARCHAR(1000)
 );
 
 CREATE TABLE topic (
-  topic_id       INTEGER AUTO_INCREMENT,
-  originator     INTEGER,
+  topic_id       INTEGER PRIMARY KEY DEFAULT nextval('seq_topic'),
+  originator     INTEGER REFERENCES expert (expert_id),
   heading        VARCHAR(100),
-  desription     VARCHAR(1000),
-  PRIMARY KEY (topic_id),
-  FOREIGN KEY (originator) REFERENCES expert (expert_id)
+  text           VARCHAR(1000)
 );
 
 
 CREATE TABLE expert_topic (
-  expert_id INTEGER,
-  topic_id  INTEGER,
-  FOREIGN KEY (expert_id) REFERENCES expert (expert_id),
-  FOREIGN KEY (topic_id)  REFERENCES topic (topic_id)
+  expert_id INTEGER REFERENCES expert (expert_id),
+  topic_id  INTEGER REFERENCES topic (topic_id)
 );
 
 CREATE TABLE subtopic (
-  superTopic INTEGER,
-  subTopic   INTEGER,
-  FOREIGN KEY (superTopic) REFERENCES topic (topic_id),
-  FOREIGN KEY (subTopic) REFERENCES topic (topic_id)
+  superTopic INTEGER REFERENCES topic (topic_id),
+  subTopic   INTEGER REFERENCES topic (topic_id)
 );
 
 -- assumption each expert writes a own recommendation
 -- Every recommendation pertains to one topic area (not several).
 CREATE TABLE recommendation (
-  description       VARCHAR(1000),
-  recommendedBy     INTEGER,
-  isRecommended     INTEGER,
-  topic             INTEGER,
-  FOREIGN KEY (topic) REFERENCES topic (topic_id),
-  FOREIGN KEY (recommendedBy) REFERENCES expert (expert_id),
-  FOREIGN KEY (isRecommended) REFERENCES expert (expert_id),
-  PRIMARY KEY (recommendedBy, isRecommended, topic)
+  recommendation_id INTEGER PRIMARY KEY DEFAULT nextval('seq_recommendation'),
+  text              VARCHAR(1000),
+  topic             INTEGER REFERENCES topic (topic_id),
+  recomendededBy    INTEGER REFERENCES expert (expert_id),
+  isRrecomendeded   INTEGER REFERENCES expert (expert_id)
 );
 
 
@@ -68,11 +55,11 @@ VALUES ('e6@expert.com', 'e6name', 'e6address', '<em>some xml description</em>')
 INSERT INTO expert (email, name, address, description)
 VALUES ('e7@expert.com', 'e7name', 'e7address', '<em>some xml description</em>');
 
-INSERT INTO topic (topic, originator, heading, text) VALUES ('area1', 1, 'heading1', 'text1');
-INSERT INTO topic (topic, originator, heading, text) VALUES ('area2', 2, 'heading2', 'text2');
-INSERT INTO topic (topic, originator, heading, text) VALUES ('area1', 1, 'heading3', 'text3');
-INSERT INTO topic (topic, originator, heading, text) VALUES ('area1', 1, 'heading4', 'text4');
-INSERT INTO topic (topic, originator, heading, text) VALUES ('area1', 1, 'heading5', 'text5');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading1', 'text1');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area2', 2, 'heading2', 'text2');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading3', 'text3');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading4', 'text4');
+INSERT INTO topic (expertise_area, originator, heading, text) VALUES ('area1', 1, 'heading5', 'text5');
 
 INSERT INTO expert_topic (expert_id, topic_id) VALUES (1, 1);
 INSERT INTO expert_topic (expert_id, topic_id) VALUES (1, 2);
@@ -86,10 +73,10 @@ INSERT INTO subtopic (superTopic, subTopic) VALUES (1, 3);
 INSERT INTO subtopic (superTopic, subTopic) VALUES (1, 4);
 INSERT INTO subtopic (superTopic, subTopic) VALUES (4, 5);
 
-INSERT INTO recommendation (description, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 1, 3);
-INSERT INTO recommendation (description, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 1, 2);
-INSERT INTO recommendation (description, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 2, 3);
-INSERT INTO recommendation (description, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 3, 4);
-INSERT INTO recommendation (description, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 2, 5);
-INSERT INTO recommendation (description, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 3, 6);
-INSERT INTO recommendation (description, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 5, 7);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 1, 3);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 1, 2);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 2, 3);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 3, 4);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 2, 5);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 3, 6);
+INSERT INTO recommendation (text, recomendededBy, isRrecomendeded) VALUES ('recommendation awesome foo', 5, 7);
